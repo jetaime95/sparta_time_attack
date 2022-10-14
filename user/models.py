0d5a95cadfc56d8437958b34e880edb778d164e1
seminju <seminju0172@gmail.com>
2022-10-14 17:27:27 +0900

    login / signup 기능 구현

--- user/models.py
+++ user/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    website = models.CharField(max_length=256, null=True)
+    address = models.CharField(max_length=256, null=True)
     phone = models.CharField(max_length=13, null=True) # -포함 010-xxxx-xxxx
--- user/views.py
+++ user/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from re import fullmatch
+from django.shortcuts import render, redirect
+from .models import User
+from django.contrib.auth import authenticate, login as loginsession
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        
+        REGEX_PHONE = '010-\d{3,4}-\d{4}'
+        if not fullmatch(REGEX_PHONE, phone):
+            return render(request, 'signup.html', {'error': '전화번호을 입력해주세요!'})
+        else:
+            User.objects.create_user(username=username, password=password, phone=phone, address=address)
+            return redirect('/login')
+
+def login(request):
+    if request.method == "GET":
+        return render(request, "login.html")
+    
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            loginsession(request, user)
+            return redirect('/home')
+        else:
+            return render('login.html')
+
+@login_required      
+def home(request):
+    user = request.user.is_authenticated
+    if user:
+        return render(request, 'home.html')
+    else:
+        return redirect('/login/')
